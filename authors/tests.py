8f113b9a96e27ff46a53b8482121d8c91baefbc2
sorkihg <sorkihg@gmail.com>
2022-07-12 17:00:05 +0900

    Refactoring
    [내 용]
    1. API모듈화 진행(utils, google_email_api)
    2. url.py 엔드포인트 수정사항 반영
    3. test code 반영

--- authors/tests.py
+++ authors/tests.py
@@ -1,16 +1,15 @@
-import json
-import bcrypt
-import jwt
+import json, jwt
 
-from django.test import TestCase, Client
-from json        import dumps
-from django.conf import settings
+from django.test        import TestCase, Client
+from django.conf        import settings
 
-from .models  import Author, User, ProposalObject, Proposal
-from unittest import mock
-from unittest.mock import patch, MagicMock
+from unittest.mock      import patch
 
-class EmailTest(TestCase):
+from authors.models     import Author, User, ProposalObject, Proposal
+from contents.models    import SubCategory, MainCategory
+
+
+class ProposalTest(TestCase):
     def setUp(self):
         User.objects.create(
             id           = 1,
@@ -19,68 +18,73 @@ class EmailTest(TestCase):
             thumbnail    = "저거.png",
             introduction = "블라블라블라블라",
         )
-
-        Author.objects.create(
-            id           = 1,
-            introduction = "블라블라",
-            career       = "블라블라블라블라",
-            user_id      = 1
+        MainCategory.objects.create(
+            id   = 1,
+            name = "출판"
         )
-
+        SubCategory.objects.create(
+            id              = 1,
+            name            = "출판기고",
+            maincategory_id = 1
+        )
+        Author.objects.create(
+            id             = 1,
+            introduction   = "블라블라",
+            career         = "블라블라블라블라",
+            user_id        = 1,
+            subcategory_id = 1
+        )
         ProposalObject.objects.create(
-            id = 1,
+            id   = 1,
             name = "출판기고",
         )
-        
         self.token = jwt.encode({'id':User.objects.get(id=1).id}, settings.SECRET_KEY, settings.ALGORITHM)
 
     def tearDown(self):
         Author.objects.all().delete()
         User.objects.all().delete()
         Proposal.objects.all().delete()
+        ProposalObject.objects.all().delete()
 
-    @patch("authors.app.GoogleEmail")
+    @patch("utils.google_email_api.GoogleEmail")
     def test_success_proposalview_post(self, mocked_requests):
         client = Client()
         data = {
-            "content"        : "블라블라",
-            "sender_email"   : "dno06101@naver.com",
-            "proposal_object": "출판기고"
+            "content"           : "블라블라",
+            "sender_email"      : "dno06101@naver.com",
+            "proposal_object_id": 1
         }
+        headers = {"HTTP_Authorization":self.token}
+        class MockedResponse:
+            def __init__(self):
+                self.credentials = {}
+            def generate_token(self, token_file_path, credentials_file_path):
+                return None
+            def send_email(self, content, author, sender_email):
+                return  {'id': '181771428a3c3f2b', 'threadId': '181771428a3c3f2b', 'labelIds': ['SENT']}
+        mocked_requests.return_value = MockedResponse()
+        response = client.post('/authors/1/proposal', json.dumps(data), content_type='application/json', **headers)
+        self.assertEqual(response.status_code, 201)
     
-        headers = {"HTTP_Authorization": self.token}
+    @patch("utils.google_email_api.GoogleEmail")
+    def test_fail_proposalview_post(self, mocked_requests):
+        client = Client()
+        data = {
+            "content"           : "블라블라",
+            "sender_email"      : "dno06101@naver.com",
+            "proposal_object_id": 2
+        }
+        self.token = jwt.encode({'id':1}, settings.SECRET_KEY, settings.ALGORITHM)
+        headers = {"HTTP_Authorization":self.token}
 
         class MockedResponse:
-            # def __init__(self):
-            #     credentials = credentials
+            def __init__(self):
+                self.credentials = {}
+            def generate_token(self, token_file_path, credentials_file_path):
+                return None
+            def send_email(self, content, author, sender_email):
+                return  {'id': '181771428a3c3f2b', 'threadId': '181771428a3c3f2b', 'labelIds': ['SENT']}
 
-            def generate_token(self, file_path):
-                return "<google.oauth2.credentials.Credentials object at 0x111a4ff10>"
-
-            file_path = 'token.json'
-
-            def send_email(self, content, author, sender_email, creds):
-                return JsonResponse({"message" : "SUCCESS"}, status=200)            
-
-            content      = "블라블라"
-            sender_email = "dno06103@naver.com"
-            author       = Author.objects.get(id=1)
-            creds        = generate_token(self, file_path)
-
-        access_token = headers["HTTP_Authorization"]
-        payload      = jwt.decode(access_token, settings.SECRET_KEY, algorithms = settings.ALGORITHM)
-        user         = User.objects.get(id = payload["id"])
-
-        Proposal.objects.create(
-            sender_email       = "dno06103@naver.com",
-            content            = "블라블라",
-            proposal_object_id = 1,
-            author_id          = 1,
-            user_id            = user.id
-        )
         mocked_requests.return_value = MockedResponse()
-
-        response = client.post('/authors/1', json.dumps(data), content_type='application/json', **headers)
-
-        self.assertEqual(response.json(), {'message' : 'SUCCESS'})
-        self.assertEqual(response.status_code, 201)
+        response = client.post('/authors/1/proposal', json.dumps(data), content_type='application/json', **headers)
+        self.assertEqual(response.status_code, 400)
